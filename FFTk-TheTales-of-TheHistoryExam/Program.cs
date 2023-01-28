59ef0ccd135f68718e451a19e3fa43979ef5f7de
markokanada <markokanadateam2@gmail.com>
2023-01-28 18:59:49 +0100

    Testing the branding options

--- FFTk-TheTales-of-TheHistoryExam/Program.cs
+++ FFTk-TheTales-of-TheHistoryExam/Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows;
 
 namespace FFTkTheTalesofTheHistoryExam
 {
     class MainClass
     {
+        public static void screenSizing()
+        {
+            double height = Console.LargestWindowHeight;
+            double Width = Console.LargestWindowWidth;
+            int maxheight = (int)Math.Round(Math.Floor(height));
+            int maxWidth = (int)Math.Round(Math.Floor(Width));
+            Console.SetWindowSize(maxheight,maxWidth);
+            Console.WriteLine($"{maxWidth},{maxheight}");
+
+        }
+
+        public static string[] Reader(string cim) {
+            StreamReader sr = new StreamReader($"{cim}.txt", Encoding.UTF8);
+            int rowcount = File.ReadAllLines($"{cim}.txt").Length;
+            string[] sorok = new string[rowcount];
+            for (int i = 0; i < rowcount; i++)
+            {
+                sorok[i] = sr.ReadLine();
+            }
+
+            return sorok;
+
+        }
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            screenSizing();
+            foreach (var item in Reader("brand"))
+            {
+                Console.WriteLine(item);
+            }
+            
         }
     }
 }
