d9e96a387b4ca238419877f39e3cfd901b9c1941
markokanada <markokanadateam2@gmail.com>
2023-01-28 18:05:41 +0100

    Initial commit

---
+++ FFTk-TheTales-of-TheHistoryExam/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FFTkTheTalesofTheHistoryExam
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
