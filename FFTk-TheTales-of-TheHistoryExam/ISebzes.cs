70e1a8960fbfa2f371c59a686e791602c15ca660
Csapii <mail.szasz.csaba@gmail.com>
2023-03-28 23:18:54 +0200

    ISebzes interface elkészült

---
+++ FFTk-TheTales-of-TheHistoryExam/ISebzes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFTkTheTalesofTheHistoryExam
+{
+    internal interface ISebzes
+    {
+        int Pancel { get; set; }
+        int SebzesMertek { get; set; }
+        void Sebzes();
+        void Elpusztitas();
+    }
+}
