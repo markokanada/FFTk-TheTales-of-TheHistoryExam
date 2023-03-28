7bf1ff6ed0ba29a13e70d89441f98d6c67933ec8
Csapii <mail.szasz.csaba@gmail.com>
2023-03-28 23:21:38 +0200

    IVedelem interface elkészült

---
+++ FFTk-TheTales-of-TheHistoryExam/IVedelem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFTkTheTalesofTheHistoryExam
+{
+    internal interface IVedelem
+    {
+        int VedelemMertek { get; set; }
+        void Vedes();
+    }
+}
