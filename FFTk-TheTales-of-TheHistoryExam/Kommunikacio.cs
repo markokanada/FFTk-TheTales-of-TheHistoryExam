a9aa0656635e95009bd63e03e2030866e049ec0d
Csapii <mail.szasz.csaba@gmail.com>
2023-04-23 19:56:06 +0200

    Kommunikacio osztály előkészítése

---
+++ FFTk-TheTales-of-TheHistoryExam/Kommunikacio.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFTkTheTalesofTheHistoryExam
+{
+    internal class Kommunikacio : IKommunikalas, IMegjelenites
+    {
+        #region IKommunikalas
+        void IKommunikalas.Beszed()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IKommunikalas.Bezaras()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IKommunikalas.Interakcio()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region IMegjelenites
+        string IMegjelenites.KarakterNev { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        string IMegjelenites.NPCNev { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        string IMegjelenites.KarakterKep { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        string IMegjelenites.NPCKep { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        string IMegjelenites.Szoveg { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        int IMegjelenites.ParbeszedIndex { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        #endregion
+    }
+}
