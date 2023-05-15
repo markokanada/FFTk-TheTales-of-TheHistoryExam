6b5a287dc49caa9ad198effcffcd514dc554c5e4
Csapii <mail.szasz.csaba@gmail.com>
2023-05-15 21:46:25 +0200

    Mentési funkciók (Mentés és Mentés törlés)

---
+++ FFTk-TheTales-of-TheHistoryExam/IMentes.cs
@@ -0,0 +1,15 @@
+﻿using FFTkTheTalesofTheHistoryExam.Szoba;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFTkTheTalesofTheHistoryExam
+{
+    internal interface IMentes
+    {
+        bool JatekMentes(Raktar raktar, Harc jatekos, SzobaFactory szoba);
+        bool MentesTorles();
+    }
+}
